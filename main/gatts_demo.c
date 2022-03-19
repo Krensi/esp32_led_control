3606ccc76380371ccce6b206ee0807b8ffe1fd02
Andreas Leitner <email@andreas-leitner.at>
2022-03-19 23:08:37 +0100

    Add LED Strip to project

--- main/gatts_demo.c
+++ main/gatts_demo.c
@@ -33,6 +33,76 @@
 
 #include "sdkconfig.h"
 
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/rmt.h"
+#include "led_strip.h"
+
+static const char *TAG = "example";
+
+led_strip_t *strip;
+
+const uint8_t led_nr = 20;
+const uint8_t led_gpio = GPIO_NUM_32;
+
+#define RMT_TX_CHANNEL RMT_CHANNEL_0
+
+#define EXAMPLE_CHASE_SPEED_MS (10)
+
+/**
+ * @brief Simple helper function, converting HSV color space to RGB color space
+ *
+ * Wiki: https://en.wikipedia.org/wiki/HSL_and_HSV
+ *
+ */
+void led_strip_hsv2rgb(uint32_t h, uint32_t s, uint32_t v, uint32_t *r, uint32_t *g, uint32_t *b)
+{
+    h %= 360; // h -> [0,360]
+    uint32_t rgb_max = v * 2.55f;
+    uint32_t rgb_min = rgb_max * (100 - s) / 100.0f;
+
+    uint32_t i = h / 60;
+    uint32_t diff = h % 60;
+
+    // RGB adjustment amount by hue
+    uint32_t rgb_adj = (rgb_max - rgb_min) * diff / 60;
+
+    switch (i) {
+        case 0:
+            *r = rgb_max;
+            *g = rgb_min + rgb_adj;
+            *b = rgb_min;
+            break;
+        case 1:
+            *r = rgb_max - rgb_adj;
+            *g = rgb_max;
+            *b = rgb_min;
+            break;
+        case 2:
+            *r = rgb_min;
+            *g = rgb_max;
+            *b = rgb_min + rgb_adj;
+            break;
+        case 3:
+            *r = rgb_min;
+            *g = rgb_max - rgb_adj;
+            *b = rgb_max;
+            break;
+        case 4:
+            *r = rgb_min + rgb_adj;
+            *g = rgb_min;
+            *b = rgb_max;
+            break;
+        default:
+            *r = rgb_max;
+            *g = rgb_min;
+            *b = rgb_max - rgb_adj;
+            break;
+    }
+}
+
 #define GATTS_TAG "GATTS_DEMO"
 
 ///Declare the static function
@@ -49,7 +119,7 @@ static void gatts_profile_b_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
 #define GATTS_DESCR_UUID_TEST_B     0x2222
 #define GATTS_NUM_HANDLE_TEST_B     4
 
-#define TEST_DEVICE_NAME            "ESP_GATTS_DEMO"
+#define TEST_DEVICE_NAME            "GEILE LED CONTROL"
 #define TEST_MANUFACTURER_DATA_LEN  17
 
 #define GATTS_DEMO_CHAR_VAL_LEN_MAX 0x40
@@ -352,6 +422,10 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
     case ESP_GATTS_WRITE_EVT: {
         ESP_LOGI(GATTS_TAG, "GATT_WRITE_EVT, conn_id %d, trans_id %d, handle %d", param->write.conn_id, param->write.trans_id, param->write.handle);
         if (!param->write.is_prep){
+            for(int i=0; i<led_nr; i++)
+                ESP_ERROR_CHECK(strip->set_pixel(strip, i, param->write.value[0], param->write.value[1], param->write.value[2]));
+
+            ESP_ERROR_CHECK(strip->refresh(strip, 100));
             ESP_LOGI(GATTS_TAG, "GATT_WRITE_EVT, value len %d, value :", param->write.len);
             esp_log_buffer_hex(GATTS_TAG, param->write.value, param->write.len);
             if (gl_profile_tab[PROFILE_A_APP_ID].descr_handle == param->write.handle && param->write.len == 2){
@@ -671,6 +745,32 @@ static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_
 
 void app_main(void)
 {
+    uint32_t red = 0;
+    uint32_t green = 0;
+    uint32_t blue = 0;
+    uint16_t hue = 0;
+    uint16_t start_rgb = 0;
+
+    uint8_t mode = 0; // 0.. RGB, 1
+
+    rmt_config_t config = RMT_DEFAULT_CONFIG_TX(led_gpio, RMT_TX_CHANNEL);
+    // set counter clock to 40MHz
+    config.clk_div = 2;
+
+    ESP_ERROR_CHECK(rmt_config(&config));
+    ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, 0));
+
+    // install ws2812 driver
+    led_strip_config_t strip_config = LED_STRIP_DEFAULT_CONFIG(led_nr, (led_strip_dev_t)config.channel);
+    strip = led_strip_new_rmt_ws2812(&strip_config);
+    if (!strip) {
+        ESP_LOGE(TAG, "install WS2812 driver failed");
+    }
+    // Clear LED strip (turn off all LEDs)
+    ESP_ERROR_CHECK(strip->clear(strip, 100));
+    // Show simple rainbow chasing pattern
+    ESP_LOGI(TAG, "LED Rainbow Chase Start");
+
     esp_err_t ret;
 
     // Initialize NVS.
